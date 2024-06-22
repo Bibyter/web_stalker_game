3281aec926db65ae55c9c5342b6f94164f1ccc86
Bibyter <nikitatkarz@gmail.com>
2024-06-22 15:56:09 +0700

    added game base template

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/vilaska')
+def vilaska():
+    return render_template('vilaska.html')
+
+@app.route('/vilaska_lobby')
+def vilaska_lobby():
+    return render_template('vilaska_lobby.html')
+
+@app.route('/vilaska_list')
+def vilaska_list():
+    return render_template('vilaska_list.html')
+
+@app.route('/vilaska_complete')
+def vilaska_complete():
+    return render_template('vilaska_complete.html')
+
 
 app.run(debug=True)
